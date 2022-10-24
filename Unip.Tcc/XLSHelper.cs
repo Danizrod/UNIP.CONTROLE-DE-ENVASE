e287f92fd940a5a1e05a253b009582fae9d7a57e
Danilo Rodrigues <danilo.rodrigues@laqus.com.br>
2022-10-24 20:27:40 -0300

    [WIP]

---
+++ Unip.Tcc/XLSHelper.cs
@@ -0,0 +1,128 @@
+﻿using OfficeOpenXml;
+using OfficeOpenXml.Style;
+
+namespace Unip.Tcc
+{
+    public class XLSHelper
+    {
+        private static readonly Color GreenBackgroundColor = ColorTranslator.FromHtml("#59bd7b");
+        private static readonly Color GrayBackgroundColor = ColorTranslator.FromHtml("#d3d3d3");
+        private static readonly Color LightRedBackgroundColor = ColorTranslator.FromHtml("#ff6666");
+        private static readonly Color RedBackgroundColor = ColorTranslator.FromHtml("#ff0000");
+
+        public static void FillBorders(ExcelRange cells)
+        {
+            cells.Style.Border.Top.Style = ExcelBorderStyle.Thin;
+            cells.Style.Border.Bottom.Style = ExcelBorderStyle.Thin;
+            cells.Style.Border.Left.Style = ExcelBorderStyle.Thin;
+            cells.Style.Border.Right.Style = ExcelBorderStyle.Thin;
+        }
+
+        public static void SetColumnsToCurrency(ExcelWorksheet sheet, int[] columns)
+        {
+            foreach (var i in columns)
+            {
+                sheet.Column(i).Style.Numberformat.Format = @"#,##0.00";
+                sheet.Column(i).Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+            }
+        }
+
+        public static void SetColumnsAlignToCenter(ExcelWorksheet sheet, int[] columns)
+        {
+            foreach (var i in columns)
+                sheet.Column(i).Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+        }
+
+        public static void SetColumnToPercentage(ExcelColumn column)
+        {
+            column.Style.Numberformat.Format = @"0.00%";
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetColumnToCenter(ExcelColumn column)
+        {
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+        }
+
+        public static void SetColumnToCurrency(ExcelColumn column)
+        {
+            column.Style.Numberformat.Format = @"#,##0.00";
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetColumnAsForward(ExcelColumn column)
+        {
+            column.Style.Numberformat.Format = @"#,##0.0000";
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetColumnAsDate(ExcelColumn column)
+        {
+            column.Style.Numberformat.Format = "dd/mm/yyyy";
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetCellsAsDate(ExcelRange cells)
+        {
+            cells.Style.Numberformat.Format = "dd/MM/yyyy";
+            cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetColumnAsForward(ExcelColumn column, int qtd)
+        {
+            var zero = "".PadLeft(qtd, '0');
+            column.Style.Numberformat.Format = $"#,##0.{zero}";
+            column.Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+        }
+
+        public static void SetRangeAsHeader(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(GreenBackgroundColor);
+            cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+        }
+
+        public static void SetRowAsSimulation(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(GrayBackgroundColor);
+        }
+
+        public static void SetRowAsOldFundSimulation(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(RedBackgroundColor);
+        }
+
+        public static void SetRowAsOldFund(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(LightRedBackgroundColor);
+        }
+
+        public static void SetRangeBorderAndBackground(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(GreenBackgroundColor);
+        }
+
+        public static void SetRangeBorderAndBackgroundMemo(ExcelRange cells)
+        {
+            cells.Style.Fill.PatternType = ExcelFillStyle.Solid;
+            cells.Style.Fill.BackgroundColor.SetColor(GrayBackgroundColor);
+        }
+
+        public static void SetAlignment(ExcelWorksheet sheet, int row, int column)
+        {
+            sheet.Cells[row, column].Style.HorizontalAlignment = ExcelHorizontalAlignment.Right;
+            sheet.Cells[row, column].Style.Border.Right.Style = ExcelBorderStyle.Thin;
+        }
+
+        public static void SetMergeCenter(ExcelRange cells)
+        {
+            cells.Merge = true;
+            cells.Style.HorizontalAlignment = ExcelHorizontalAlignment.Center;
+            cells.Style.VerticalAlignment = ExcelVerticalAlignment.Center;
+        }
+    }
+}
--- Unip.Tcc/frmEstatisticas.cs
+++ Unip.Tcc/frmEstatisticas.cs
@@ -1,5 +1,8 @@
-﻿using System.IO.Ports;
-using static System.Net.Mime.MediaTypeNames;
+﻿using DocumentFormat.OpenXml.Bibliography;
+using DocumentFormat.OpenXml.Office2016.Excel;
+using OfficeOpenXml;
+using System.IO.Packaging;
+
 
 namespace Unip.Tcc
 {
@@ -118,7 +121,73 @@ namespace Unip.Tcc
 
         private void GenerateExcel(object sender, EventArgs e)
         {
+            var data = DateTime.Now.Day + "-" + DateTime.Now.Month + "-" + DateTime.Now.Year;
+            var horario = DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second;
+            using SaveFileDialog saveFileDialog = new()
+            {
+                Filter = "Planilha Excel | *.xlsx*",
+                AddExtension = true,
+                FileName = "Dados_Coletados_" + data + "_as_" + horario
+            };
 
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    var fileInfo = new FileInfo(saveFileDialog.FileName + ".xlsx");
+                    ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+                    using ExcelPackage package = new(fileInfo);
+
+                    CriaPastaDeTrabalho(package);
+
+                    package.Save();
+                }
+                catch (Exception ex)
+                {
+
+                    MessageBox.Show(ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        private static void CriaPastaDeTrabalho(ExcelPackage package)
+        {
+            var sheet = package.Workbook.Worksheets.Add("Dados Coletados");
+            CriaCabecalhos(sheet);
+
+
+
+            var row = 4;
+
+            sheet.Cells[1, 1, 7, 7].AutoFitColumns();
+        }
+
+        private static void CriaCabecalhos(ExcelWorksheet sheet)
+        {
+            sheet.Cells["A1"].Value = "Relatório";
+            sheet.Cells["B1"].Value = "Dados Importados";
+
+            sheet.Cells["D1"].Value = "Data";
+            sheet.Cells["E1"].Value = DateTime.Now;
+            sheet.Cells["E1"].Style.Numberformat.Format = "dd/MM/yyyy";
+
+            sheet.Cells["B3"].Value = "Garrafa 200mL";
+            sheet.Cells["C3"].Value = "Garrafa 300mL";
+            sheet.Cells["A4"].Value = "Quantidade Produzida";
+            sheet.Cells["A5"].Value = "Quantidade Rejeitada";
+            sheet.Cells["A6"].Value = "Energia Gasta";
+            sheet.Cells["A7"].Value = "Litros Envazados";
+
+            XLSHelper.SetRangeBorderAndBackground(sheet.Cells["A1"]);
+            XLSHelper.FillBorders(sheet.Cells["A1:B1"]);
+            XLSHelper.SetRangeBorderAndBackground(sheet.Cells["D1"]);
+            XLSHelper.FillBorders(sheet.Cells["D1:E1"]);
+
+            XLSHelper.SetRangeAsHeader(sheet.Cells[3, 1, 3, 3]);
+            XLSHelper.FillBorders(sheet.Cells[3, 1, 3, 3]);
+
+            XLSHelper.SetRangeAsHeader(sheet.Cells[3, 1, 7, 1]);
+            XLSHelper.FillBorders(sheet.Cells[3, 1, 7, 1]);
         }
     }
 }
